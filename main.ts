bd7f3d7d4042d1694941ce88462a34abd715be25
vdang <dbacvan@gmail.com>
2022-11-06 16:54:12 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,8 +3,7 @@ namespace controller {
     export function isLeftJoystickClicked() {
         return pins.digitalReadPin(DigitalPin.P8) == 0
     }
-    //% block
-    export function leftJoystickX () {
+    function leftJoystickX () {
         return 2 - Math.round(pins.map(
         pins.analogReadPin(AnalogPin.P1),
         0,
@@ -15,14 +14,13 @@ namespace controller {
     }
     //% block
     export function isLeftJoystickMovedLeft() {
-        return leftJoystickX() == -2
+        return leftJoystickX() < 0
     }
     //% block
     export function isLeftJoystickMovedRight() {
-        return leftJoystickX() == 2
+        return leftJoystickX() > 0
     }
-    //% block
-    export function leftJoystickY() {
+    function leftJoystickY() {
         return 2 - Math.round(pins.map(
             pins.analogReadPin(AnalogPin.P0),
             0,
@@ -33,18 +31,17 @@ namespace controller {
     }
     //% block
     export function isLeftJoystickMovedDown() {
-        return leftJoystickY() == -2
+        return leftJoystickY() < 0
     }
     //% block
     export function isLeftJoystickMovedUp() {
-        return leftJoystickY() == 2
+        return leftJoystickY() > 0
     }
     //% block
     export function isRightJoystickClicked() {
         return pins.digitalReadPin(DigitalPin.P9) == 0
     }
-    //% block
-    export function rightJoystickX () {
+    function rightJoystickX () {
         return 2 - Math.round(pins.map(
         pins.analogReadPin(AnalogPin.P3),
         0,
@@ -55,14 +52,13 @@ namespace controller {
     }
     //% block
     export function isRightJoystickMovedLeft() {
-        return rightJoystickX() == -2
+        return rightJoystickX() < 0
     }
     //% block
     export function isRightJoystickMovedRight() {
-        return rightJoystickX() == 2
+        return rightJoystickX() > 0
     }
-    //% block
-    export function rightJoystickY () {
+    function rightJoystickY () {
         return 2 - Math.round(pins.map(
         pins.analogReadPin(AnalogPin.P2),
         0,
@@ -73,11 +69,11 @@ namespace controller {
     }
     //% block
     export function isRightJoystickMovedDown() {
-        return rightJoystickY() == -2
+        return rightJoystickY() < 0
     }
     //% block
     export function isRightJoystickMovedUp() {
-        return rightJoystickY() == 2
+        return rightJoystickY() > 0
     }
     pins.setPull(DigitalPin.P8, PinPullMode.PullUp)
     pins.setPull(DigitalPin.P9, PinPullMode.PullUp)
